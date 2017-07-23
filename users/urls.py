6efe3f67cdefb2bfc19ca5e60102f4771e5678c1
cloudPai <1565730756@qq.com>
2017-07-23 20:26:13 +0800

    19.2.2  登录界面 done

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+"""为应用程序定义URL模式"""
+
+from django.conf.urls import url
+from django.contrib.auth.views import login
+
+from  . import views
+
+urlpatterns = [
+    #登录界面
+    url(r'^login/$', login, {'template_name': 'users/login.html'},
+        name='login'),
+
+]
